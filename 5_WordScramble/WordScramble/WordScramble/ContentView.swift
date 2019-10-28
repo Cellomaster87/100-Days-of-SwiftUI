97f39b316ec06009ea193e5ebae49a4fb831deac
Michele Galvagno <michicello@gmail.com>
2019-10-28 17:15:23 +0100

    P5 beginning

---
+++ 5_WordScramble/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Michele Galvagno on 28/10/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
